8bea65b63e3be02dec1485f5eef40b2e228d457e
Jeong-AYeong <87751593+Jeong-AYeong@users.noreply.github.com>
2022-11-27 21:57:03 +0900

    Update practice15_practice_class2.cpp
    add question

--- practice15_practice_class2.cpp
+++ practice15_practice_class2.cpp
@@ -1,3 +1,12 @@
+/*******************************************************************************************************************************************
+1. Linked list with classes [Normal]
+- Make the linked list as a class. This code will be used for next daily practices.
+- 링크드리스트를 클래스로 만드세요! 요건 다음에도 사용될 예정이니 코드를 잘 보관하세요!
+
+- It should support append and next functions as the member and they are chainable (append와 next 함수를 지원해야하며, 체이닝이 되어야 합니다)
+- It should remember the length of the list, current(last) referencing position (이것은 리스트의 길이 마지막으로 참조한 위치를 기억해야합니다)
+- If a rewind function is called, it goes back to the first position of the list (Rewind 함수가 호출되면 리스트의 처음으로 돌아갑니다).
+*******************************************************************************************************************************************/
 #include <iostream>
 using namespace std;
 
